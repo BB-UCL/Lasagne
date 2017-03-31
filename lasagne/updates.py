2a394f7aa1b84801d9267e615dfa503ba21f1057
Alex Botev <botevmg@gmail.com>
2017-03-31 14:50:45 +0100

    Small additions.

--- lasagne/updates.py
+++ lasagne/updates.py
@@ -581,7 +581,7 @@ def adam(loss_or_grads, params, learning_rate=0.001, beta1=0.9,
            arXiv preprint arXiv:1412.6980.
     """
     all_grads = get_or_compute_grads(loss_or_grads, params)
-    t_prev = theano.shared(utils.floatX(0.))
+    t_prev = theano.shared(utils.floatX(0.), name="Adam_t")
     updates = OrderedDict()
 
     # Using theano constant to prevent upcasting of float32
@@ -593,9 +593,11 @@ def adam(loss_or_grads, params, learning_rate=0.001, beta1=0.9,
     for param, g_t in zip(params, all_grads):
         value = param.get_value(borrow=True)
         m_prev = theano.shared(np.zeros(value.shape, dtype=value.dtype),
-                               broadcastable=param.broadcastable)
+                               broadcastable=param.broadcastable,
+                               name="Adam_mu::" + param.name)
         v_prev = theano.shared(np.zeros(value.shape, dtype=value.dtype),
-                               broadcastable=param.broadcastable)
+                               broadcastable=param.broadcastable,
+                               name="Adam_var::" + param.name)
 
         m_t = beta1*m_prev + (one-beta1)*g_t
         v_t = beta2*v_prev + (one-beta2)*g_t**2
